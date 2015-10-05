cd737d2ecf99bed25100b655124c7b0ddde1d6a2
chrjoh <christerjoha@gmail.com>
2015-10-05 19:02:06 +0200

    added ntlm offsets as var

--- main.go
+++ main.go
@@ -22,6 +22,39 @@ type ChallengeResponse struct {
 	Response  string
 }
 
+var (
+	NTLM_SIG_OFFSET  = 0
+	NTLM_TYPE_OFFSET = 8
+
+	NTLM_TYPE1_FLAGS_OFFSET   = 12
+	NTLM_TYPE1_DOMAIN_OFFSET  = 16
+	NTLM_TYPE1_WORKSTN_OFFSET = 24
+	NTLM_TYPE1_DATA_OFFSET    = 32
+	NTLM_TYPE1_MINSIZE        = 16
+
+	NTLM_TYPE2_TARGET_OFFSET     = 12
+	NTLM_TYPE2_FLAGS_OFFSET      = 20
+	NTLM_TYPE2_CHALLENGE_OFFSET  = 24
+	NTLM_TYPE2_CONTEXT_OFFSET    = 32
+	NTLM_TYPE2_TARGETINFO_OFFSET = 40
+	NTLM_TYPE2_DATA_OFFSET       = 48
+	NTLM_TYPE2_MINSIZE           = 32
+
+	NTLM_TYPE3_LMRESP_OFFSET     = 12
+	NTLM_TYPE3_NTRESP_OFFSET     = 20
+	NTLM_TYPE3_DOMAIN_OFFSET     = 28
+	NTLM_TYPE3_USER_OFFSET       = 36
+	NTLM_TYPE3_WORKSTN_OFFSET    = 44
+	NTLM_TYPE3_SESSIONKEY_OFFSET = 52
+	NTLM_TYPE3_FLAGS_OFFSET      = 60
+	NTLM_TYPE3_DATA_OFFSET       = 64
+	NTLM_TYPE3_MINSIZE           = 52
+
+	NTLM_BUFFER_LEN_OFFSET    = 0
+	NTLM_BUFFER_MAXLEN_OFFSET = 2
+	NTLM_BUFFER_OFFSET_OFFSET = 4
+	NTLM_BUFFER_SIZE          = 8
+)
 var serverResponse = make(map[uint32]string)
 var serverResponsePairs = []ChallengeResponse{}
 
@@ -70,15 +103,6 @@ func handlePacket(packet gopacket.Packet) {
 					})
 				}
 			}
-			//		data, err := base64.StdEncoding.DecodeString(base)
-			//		if err != nil {
-			//			fmt.Println("error:", err)
-			//			return
-			//		}
-
-			//	fmt.Printf("%q\n", hex.Dump(data))
-			//	fmt.Println("-----------------------------------------------------------------------------------------")
-			//	}
 		}
 	}
 }
@@ -91,16 +115,37 @@ func response(s string) bool {
 	return regExpRes.FindString(s) != ""
 }
 
+// ntlmv2 challenge type 2
+// 0  NTLMSSP Signature
+// 8  NTLM Message Type
+// 12 Target Name
+// 20 Flags
+// 24 Challenge
+// 32 end of header, start of optional data blocks
+
+// ntlmv2 response type 3
+//0 NTLMSSP Signature Null-terminated ASCII "NTLMSSP" (0x4e544c4d53535000)
+//8 NTLM Message Type long (0x03000000)
+//12 LM/LMv2 Response security buffer
+//20 NTLM/NTLMv2 Response security buffer
+//28 Target Name security buffer
+//36 User Name security buffer
+//44 Workstation Name security buffer
+
 func dumpPairs() {
 	for _, pair := range serverResponsePairs {
 		dataCallenge, _ := base64.StdEncoding.DecodeString(pair.Challenge)
 		dataResponse, _ := base64.StdEncoding.DecodeString(pair.Response)
+
 		fmt.Println("======================================================")
 		//fmt.Println(base64.StdEncoding.DecodeString(pair.Challenge))
-		fmt.Println(hex.EncodeToString(dataCallenge))
+		//offset to the challenge, 8 bytes long
+		fmt.Println(hex.EncodeToString(dataCallenge[NTLM_TYPE2_CHALLENGE_OFFSET:NTLM_TYPE2_CONTEXT_OFFSET]))
 		fmt.Println("------------------------------------------------------")
 		//fmt.Println(base64.StdEncoding.DecodeString(pair.Response))
 		fmt.Println(hex.EncodeToString(dataResponse))
+		fmt.Println("::::::::::::::::::::::::")
+		fmt.Println(hex.EncodeToString(dataResponse[:44]))
 		fmt.Println("======================================================")
 	}
 }
